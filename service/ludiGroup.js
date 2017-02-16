5df8dba63162aafd51c5c83ed902088165e61946
grensley <grensley@gmail.com>
2017-02-15 20:14:30 -0700

    Added ludiGroups.js

---
+++ service/ludiGroup.js
@@ -0,0 +1,163 @@
+'use strict';
+// public api
+var ludiGroup = {
+  find: function(req, res, next){
+    req.query.name = req.query.name ? req.query.name : '';
+    req.query.limit = req.query.limit ? parseInt(req.query.limit, null) : 20;
+    req.query.page = req.query.page ? parseInt(req.query.page, null) : 1;
+    req.query.sort = req.query.sort ? req.query.sort : '_id';
+
+    var filters = {};
+    if (req.query.name) {
+      filters.name = new RegExp('^.*?'+ req.query.name +'.*$', 'i');
+    }
+
+    req.app.db.models.LudiGroup.pagedFind({
+      filters: filters,
+      keys: 'name',
+      limit: req.query.limit,
+      page: req.query.page,
+      sort: req.query.sort
+    }, function(err, results) {
+      if (err) {
+        return next(err);
+      }
+      results.filters = req.query;
+      res.status(200).json(results);
+    });
+  },
+
+  read: function(req, res, next){
+    req.app.db.models.LudiGroup.findById(req.params.id).exec(function(err, ludiGroup) {
+      if (err) {
+        return next(err);
+      }
+      res.status(200).json(ludiGroup);
+    });
+  },
+
+  create: function(req, res, next){
+    var workflow = req.app.utility.workflow(req, res);
+
+    workflow.on('validate', function() {
+
+      if (!req.body.category) {
+        workflow.outcome.errors.push('Missing category information');
+        return workflow.emit('response');
+      }
+
+      workflow.emit('createLudiGroup');
+    });
+
+    workflow.on('createLudiGroup', function() {
+      var fieldsToSet = {
+        _id: req.app.utility.slugify(req.body.name),
+        name: req.body.name
+      };
+
+      req.app.db.models.LudiGroup.create(fieldsToSet, function(err, ludiGroup) {
+        if (err) {
+          return workflow.emit('exception', err);
+        }
+
+        workflow.outcome.record = ludiGroup;
+        return workflow.emit('response');
+      });
+    });
+
+    workflow.emit('validate');
+  },
+
+  update: function(req, res, next){
+    var workflow = req.app.utility.workflow(req, res);
+
+    workflow.on('validate', function() {
+      if (!req.body.name) {
+        workflow.outcome.errfor.name = 'required';
+        return workflow.emit('response');
+      }
+
+      workflow.emit('patchLudiGroup');
+    });
+
+    workflow.on('patchLudiGroup', function() {
+      var fieldsToSet = {
+        name: req.body.name
+      };
+      var options = { new: true };
+      req.app.db.models.LudiGroup.findByIdAndUpdate(req.params.id, fieldsToSet, options, function(err, ludiGroup) {
+        if (err) {
+          return workflow.emit('exception', err);
+        }
+
+        workflow.outcome.ludiGroup = ludiGroup;
+        return workflow.emit('response');
+      });
+    });
+
+    workflow.emit('validate');
+  },
+
+  permissions: function(req, res, next){
+    var workflow = req.app.utility.workflow(req, res);
+
+    workflow.on('validate', function() {
+      if (!req.user.roles.admin.isMemberOf('root')) {
+        workflow.outcome.errors.push('You may not change the permissions of groups.');
+        return workflow.emit('response');
+      }
+
+      if (!req.body.permissions) {
+        workflow.outcome.errfor.permissions = 'required';
+        return workflow.emit('response');
+      }
+
+      workflow.emit('patchLudiGroup');
+    });
+
+    workflow.on('patchLudiGroup', function() {
+      var fieldsToSet = {
+        permissions: req.body.permissions
+      };
+      var options = { new: true };
+
+      req.app.db.models.LudiGroup.findByIdAndUpdate(req.params.id, fieldsToSet, options, function(err, ludiGroup) {
+        if (err) {
+          return workflow.emit('exception', err);
+        }
+
+        workflow.outcome.ludiGroup = ludiGroup;
+        return workflow.emit('response');
+      });
+    });
+
+    workflow.emit('validate');
+  },
+
+  delete: function(req, res, next){
+    var workflow = req.app.utility.workflow(req, res);
+
+    workflow.on('validate', function() {
+      if (!req.user.roles.admin.isMemberOf('root')) {
+        workflow.outcome.errors.push('You may not delete groups.');
+        return workflow.emit('response');
+      }
+
+      workflow.emit('deleteLudiGroup');
+    });
+
+    workflow.on('deleteLudiGroup', function(err) {
+      req.app.db.models.LudiGroup.findByIdAndRemove(req.params.id, function(err, ludiGroup) {
+        if (err) {
+          return workflow.emit('exception', err);
+        }
+
+        workflow.emit('response');
+      });
+    });
+
+    workflow.emit('validate');
+  }
+
+};
+module.exports = ludiGroup;
